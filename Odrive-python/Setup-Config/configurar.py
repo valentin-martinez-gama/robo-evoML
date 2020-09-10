5c984c5eb756ea71aaff055bd7bb2937eb298cde
valemgama <valentin.martinez.gama@gmail.com>
2020-09-10 12:40:12 -0500

    fix configurar velocidad

--- Odrive-python/Setup-Config/configurar.py
+++ Odrive-python/Setup-Config/configurar.py
@@ -31,7 +31,7 @@ def corrientes(odrv, limiteCorriente = 10, calibracion = 10):
         print("Reiniciando para fijar corriente MOSFET >60")
         return odrv.reboot()
     return"Nuevas corrientes fijadas exitosamente"
-    
-def limite_velocidad(odrv, limvel = encoderCPR):
+
+def limite_velocidad(odrv, limiteVelocidad = encoderCPR):
     odrv.axis0.controller.config.vel_limit = limiteVelocidad
     odrv.axis1.controller.config.vel_limit = limiteVelocidad
