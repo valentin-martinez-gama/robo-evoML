a271f886e46326d93175a97e84b76546e163e851
edulew <70707792+edulew@users.noreply.github.com>
2020-10-13 15:00:17 -0500

    Add build pandas function

--- Odrive-python/pandas/pandas_setup.py
+++ Odrive-python/pandas/pandas_setup.py
@@ -1,4 +1,23 @@
 # Script test
 import pandas as pd
 
-df = pd.DataFrame()
+def build_pandas(samples):
+    df = pd.DataFrame()
+    # Create columns to store data
+    id = pd.Series([], dtype=int)
+    pos_gain = pd.Series([], dtype=float)
+    vel_gain = pd.Series([], dtype=float)
+    vel_integrator_gain = pd.Series([], dtype=float)
+    df.insert(0, "Iteracion", id)
+    df.insert(1, "pos_gain", pos_gain)
+    df.insert(2, "vel_gain", vel_gain)
+    df.insert(3, "vel_integrator_gain", vel_integrator_gain)
+    for i in range(samples):
+        actual_pos = "pos_estimate" + str(i)
+        desired_pos = "input_pos" + str(i)
+        df.insert(i+4, actual_pos, pd.Series([], dtype=float))
+        df.insert(2*i+5, desired_pos, pd.Series([], dtype=float))
+    return df
+
+def data_aquisition():
+    for i in range(samples):
