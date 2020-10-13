8e54ad1e24657d9d6024fd9a94f377c0986ed9b7
edulew <70707792+edulew@users.noreply.github.com>
2020-10-13 17:52:30 -0500

    Add calculate_error function

--- Odrive-python/data/pandas_setup.py
+++ Odrive-python/data/pandas_setup.py
@@ -24,6 +24,27 @@ def add_pandas_entry(df, id, kp, kv, kvi, estimates, inputs):
     df.loc[len(df.index)] = row
     return df
 
+def calculate_error(data, samples):
+    df = pd.DataFrame()
+    df = data.iloc[:, :4]
+    positive_error = pd.Series([], dtype=float)
+    negative_error = pd.Series([], dtype=float)
+    for i in range(len(data)):
+        pos_error = 0.0
+        neg_error = 0.0
+        for s in range(samples):
+            error = data["input_pos" + str(s)][i] - data["pos_estimate" + str(s)][i]
+            if error > 0:
+                pos_error += error
+            else:
+                neg_error += error
+        positive_error[i] = pos_error
+        negative_error[i] = neg_error
+    df.insert(4, "positive_error", positive_error)
+    df.insert(5, "negative_error", negative_error)
+    return df
+
+
 def csv_export(df):
     csv_name = input("Introduce el nombre con el que quieres guardar el archivo (SIN .csv): ")
     df.to_csv(rf"{csv_name}.csv", index = False)
