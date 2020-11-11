2dedec81b9fa025b56aff67c9b54715121a70aea
valemgama <valentin.martinez.gama@gmail.com>
2020-11-11 01:04:38 -0600

    working error calculation for individual

--- Odrive-python/evo_robo.py
+++ Odrive-python/evo_robo.py
@@ -15,7 +15,7 @@ import trajectory
 ### DESCONTAR EL TIMEPO X OPERACION EN CADA UNA
 traj = trajectory.build_trajectory(pos1=0, pos2=pi, t1=0.5, t2=0.5, res=100)
 samples = 50
-num_individuals = 1
+num_individuals = 5
 num_generations = 1
 
 def evo_gains(odrv):
@@ -32,16 +32,20 @@ def evo_gains(odrv):
     population = []
     #Initiate population randomly
     for n in range(1, num_individuals+1):
-        population.append(Individual(0, n, (r_uni(10,80), r_uni(1/10,300/10), r_uni(0,1))))
+        population.append(Individual(0, n, (r_uni(10,70), r_uni(1/10,3/10), r_uni(0,.75))))
 
+    configure.gains(odrv)
 
 def get_error_score(odrv, gains, traj):
     configure.gains(odrv, *gains)
-    t_df = pd.Series(data=performance_traj(odrv, traj, samples))
-    print(t_df)
-    return 1
-    #error = sum(np.subtract(t_df.at[""]))
-
+    ok = 1 #test_vibration()
+    if ok:
+        t_df = pd.Series(data=performance_traj(odrv, traj, samples))
+        error = sum(np.square(np.subtract(t_df.at["input_pos"],t_df.at["pos_estimate_a0"]))) + sum(np.square(np.subtract(t_df.at["input_pos"],t_df.at["pos_estimate_a1"])))
+        print(error)
+    else:
+        error = 0
+    return error
 
 
 def performance_traj(odrv, traj, samples=0):
@@ -53,13 +57,15 @@ def performance_traj(odrv, traj, samples=0):
     ret_time = traj["RET_PERIOD"]
     sample_diff = len(traj["OUTBOUND"])%sample_interval
 
-    inputs = traj["OUTBOUND"] + traj["RETURN"]
+    inputs = []
     estimates_a0 = []
     estimates_a1 = []
     currents_a0 = []
     currents_a1 = []
 
     directions = (traj["OUTBOUND"], traj["RETURN"])
+
+    start = time.perf_counter()
     for d_traj in directions:
         if d_traj== traj["OUTBOUND"]:
             T_time = traj["OUT_PERIOD"]
@@ -70,6 +76,7 @@ def performance_traj(odrv, traj, samples=0):
             odrv.axis0.controller.input_pos = p
             odrv.axis1.controller.input_pos = p
             if ((i-1)%sample_interval == sample_interval-1):
+                inputs.append(p)
                 estimates_a0.append(odrv.axis0.encoder.pos_estimate)
                 currents_a0.append(odrv.axis0.motor.current_control.Iq_setpoint)
                 estimates_a1.append(odrv.axis1.encoder.pos_estimate)
@@ -77,7 +84,8 @@ def performance_traj(odrv, traj, samples=0):
                 time.sleep(float(T_time-robo.input_sleep_adjust-robo.data_delay))
             else:
                 time.sleep(float(T_time-robo.input_sleep_adjust))
-
+    end = time.perf_counter()
+    print("TRAYECTORY TIME = " + str(end-start))
     return {"input_pos":inputs,
     "pos_estimate_a0":estimates_a0,
     "pos_estimate_a1":estimates_a1,
