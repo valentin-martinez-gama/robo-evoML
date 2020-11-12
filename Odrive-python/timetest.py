9ea99bb9aa55d698df637d3e3d31fcf393383109
valemgama <valentin.martinez.gama@gmail.com>
2020-11-12 05:03:52 -0600

    reduce timetest test times

--- Odrive-python/timetest.py
+++ Odrive-python/timetest.py
@@ -9,7 +9,7 @@ def check_sleep(amount):
     end = time.perf_counter()
     return end-start
 
-def get_sleep_error(mint=1, maxt=4+1, tinter=1):
+def get_sleep_error(mint=2, maxt=4+1, tinter=1):
     errs = []
     for sleep_time in range(mint, maxt, tinter):
         error = sum(check_sleep(sleep_time/1000)-sleep_time/1000 for i in range(100))*8
@@ -23,7 +23,7 @@ def get_sleep_error(mint=1, maxt=4+1, tinter=1):
 def get_input_pos_delay(odrv, iters=100):
     delays = []
 
-    outbound = [i*.5/(iters//2) for i in range(0, iters//2)]
+    outbound = [i*.25/(iters//2) for i in range(0, iters//2)]
     ret = list(outbound)
     ret.reverse()
     points = (outbound+ret)
@@ -34,7 +34,7 @@ def get_input_pos_delay(odrv, iters=100):
         odrv.axis1.controller.input_pos = p
         end = time.perf_counter()
         delays.append(end-start)
-        time.sleep(1/iters)
+        time.sleep(.01)
 
     odrv.axis0.controller.input_pos = 0
     odrv.axis1.controller.input_pos = 0
@@ -44,12 +44,11 @@ def get_input_pos_delay(odrv, iters=100):
     return input_del
 
 def get_info_read_delay(odrv, iters=100):
-    traj = trajectory.build_trajectory(res=iters)
 
-    sample_interval = (len(traj["OUTBOUND"])+len(traj["RETURN"]))//iters
-    out_time = traj["OUT_PERIOD"]
-    ret_time = traj["RET_PERIOD"]
-    sample_diff = len(traj["OUTBOUND"])%sample_interval
+    outbound = [i*.25/(iters//2) for i in range(0, iters//2)]
+    ret = list(outbound)
+    ret.reverse()
+    points = (outbound+ret)
 
     estimates_a0 = []
     estimates_a1 = []
@@ -58,10 +57,10 @@ def get_info_read_delay(odrv, iters=100):
     currents_a1 = []
     delays = []
 
-    for i, p in enumerate(traj["OUTBOUND"]):
+    for i, p in enumerate(points):
         odrv.axis0.controller.input_pos = p
         odrv.axis1.controller.input_pos = p
-        time.sleep(out_time)
+        time.sleep(.01)
         if ((i-1)%sample_interval == sample_interval-1):
             start = time.perf_counter()
             inputs.append(p)
@@ -71,20 +70,10 @@ def get_info_read_delay(odrv, iters=100):
             currents_a1.append(odrv.axis1.motor.current_control.Iq_setpoint)
             end = time.perf_counter()
             delays.append(end-start)
-    for i, p in enumerate(traj["RETURN"]):
-        odrv.axis0.controller.input_pos = p
-        odrv.axis1.controller.input_pos = p
-        time.sleep(ret_time)
-        if ((i-1+sample_diff)%sample_interval == sample_interval-1):
-            start = time.perf_counter()
-            inputs.append(p)
-            estimates_a0.append(odrv.axis0.encoder.pos_estimate)
-            currents_a0.append(odrv.axis0.motor.current_control.Iq_setpoint)
-            estimates_a1.append(odrv.axis1.encoder.pos_estimate)
-            currents_a1.append(odrv.axis1.motor.current_control.Iq_setpoint)
-            end = time.perf_counter()
-            delays.append(end-start)
 
+    odrv.axis0.controller.input_pos = 0
+    odrv.axis1.controller.input_pos = 0
+    
     read_del = sum(delays)/len(delays)
     print("Average read_info execution time is %0.5fms" % (read_del*1000))
     return read_del
